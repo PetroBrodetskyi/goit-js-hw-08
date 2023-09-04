97fedaefc77039174c3588fbcdbae86a674df83c
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-09-04 23:20:18 +0200

    first task work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryElem = galleryItems
+    .map(({ preview, description, original }) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`)
+.join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryElem)
+
+galleryContainer.addEventListener('click', imgClick)
+
+function imgClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const modal = SimpleLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="800" height="600">`
+    );
+    
+    modal.show();
+
+    function closeModal(evt) {
+        if (evt.code === 'Escape') {
+            modal.close();
+            document.removeEventListener('keydown', closeModal);
+        }
+    }
+    document.addEventListener('keydown', closeModal);
+    
+};
