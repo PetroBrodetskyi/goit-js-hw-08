8a1e4a34a972128893b746da91d452669a5b6ada
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-09-05 14:30:18 +0200

    SecondTaskDraft

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo('vimeo-player', {
+  id: 236203659,
+  width: 640,
+  height: 360,
+});
+
+player.on('timeupdate', throttle(({ seconds }) => {
+  localStorage.setItem('video-current-time', seconds);
+}, 1000));
+
+const savedTime = localStorage.getItem('video-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+};
